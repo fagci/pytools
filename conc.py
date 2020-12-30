62072725428db41a6a0a7926e5222eb991f08f85
Mikhail Yudin <fagci.nsk@gmail.com>
2020-12-30 17:35:17 +0700

    rm unnecessary module for now

--- conc.py
+++
@@ -1,7 +0,0 @@
-from concurrent.futures import ThreadPoolExecutor
-from tqdm import tqdm
-
-def thread_exec(fn, iterable, unit='it', threads='4'):
-    with ThreadPoolExecutor(threads) as execuutor:
-        return tqdm(execuutor.map(fn, iterable), total=len(iterable), unit=unit)
-
