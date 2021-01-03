f3d9ba11e6bd0c7cc2947e5e4d4e64e58c27ee85
Mikhail Yudin <fagci.nsk@gmail.com>
2021-01-03 14:29:24 +0700

    code style

--- lib/search.py
+++ lib/search.py
@@ -2,17 +2,20 @@ from requests import get
 from bs4 import BeautifulSoup
 
 def yandex(query):
-    r = get('https://yandex.ru/search/', {'text':query})
-    soup = BeautifulSoup(r.text, 'lxml')
+    response = get('https://yandex.ru/search/', {'text':query})
+    soup = BeautifulSoup(response.text, 'lxml')
+
     res = []
-    for l in soup.find_all('li', class_='serp-item'):
+
+    for item in soup.find_all('li', class_='serp-item'):
         result = {
-            'url': l.a['href'],
-            'name': l.a.text,
+            'url': item.a['href'],
+            'name': item.a.text,
         }
-        text = l.find('div', class_="text-container")
-        if(text):
+        text = item.find('div', class_="text-container")
+        if text:
             result['text'] = text.text
         res.append(result)
+
     return res
 
