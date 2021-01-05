f5271eb88676805f164695cc71660c40afc2ebe0
Mikhail Yudin <fagci.nsk@gmail.com>
2021-01-05 23:09:07 +0700

    added domain to resolve if needed

--- modules/dns.py
+++ modules/dns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from concurrent.futures import ThreadPoolExecutor as TPE
+from concurrent.futures import ThreadPoolExecutor
 from re import compile
 from time import time
 
@@ -14,7 +14,8 @@ ipv4_re = compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
 class Dns:
     """Perform DNS related actions"""
 
-    def _get_nameservers(self) -> list:
+    @staticmethod
+    def _get_nameservers() -> list:
         url = 'https://public-dns.info/nameservers.csv'
         lines = get(url).text.splitlines(False)
         col_names = lines[0].split(',')
@@ -48,20 +49,20 @@ class Dns:
         resolver.nameservers = [nameserver['ip_address']]
         try:
             t = time()
-            res: Answer = resolver.resolve('ya.ru', lifetime=0.1)
+            res: Answer = resolver.resolve(
+                self.domain_to_resolve, lifetime=0.1)
             if len(res.rrset) > 0:
                 nameserver['time'] = round((time() - t) * 1000)
         except:
             pass
         return nameserver
 
-    def fastest(self):
-        """
-        Retrieves ipv4 dns nameservers,
-        and get fastest (<100ms) for your location/network.
-        """
+    def fastest(self, domain_to_resolve='ya.ru'):
+        """Retrieves ipv4 dns nameservers,
+        and get fastest (<100ms) for your location/network."""
+        self.domain_to_resolve = domain_to_resolve
         nameservers = self._get_nameservers()
-        with TPE() as executor:
+        with ThreadPoolExecutor() as executor:
             results = []
             for res in tqdm(executor.map(self._check, nameservers), total=len(nameservers)):
                 tim = res.get('time')
