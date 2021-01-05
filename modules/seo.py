e6182fa59000c2e4c190707f1621ca9ca7c7fde8
Mikhail Yudin <fagci.nsk@gmail.com>
2021-01-06 01:00:24 +0700

    better seo check functions

--- modules/seo.py
+++ modules/seo.py
@@ -6,6 +6,7 @@ from urllib.parse import urlparse
 
 import bs4
 import requests
+from requests.models import Response
 from tqdm import tqdm
 
 OK_CODES = [200]
@@ -15,15 +16,72 @@ TITLE_MAX_LENGTH = 70
 DESCRIPTION_MIN_LENGTH = 70
 DESCRIPTION_MAX_LENGTH = 200
 
-checks = {
-    'len_title': lambda t: TITLE_MIN_LENGTH < len(t) < TITLE_MAX_LENGTH,
-    'len_desc': lambda d: DESCRIPTION_MIN_LENGTH < len(d) < DESCRIPTION_MAX_LENGTH,
-    'ttfb': lambda t: t < MAX_TTFB,
-}
-
 
 class Seo:
     """SEO tools"""
+
+    def check(self, sitemap_url, t=4, o=''):
+        """Performs base technical SEO check for urls from sitemap
+
+        sitemap_url -- url pointing to sitemap.xml
+        t -- number of threads
+        Keep in mind: when more threads,
+        ttfb may be larger than MAX_TTFB and test will fail
+        """
+        self.checks = {}
+        for func in dir(self):
+            if func.startswith('check_'):
+                fn = getattr(self, func)
+                if callable(fn):
+                    self.checks[func[6:]] = fn
+
+        sys.stderr.write('Check sitemap...\n')
+        xml = self._get_page(self._get_response(sitemap_url))
+        urls = [a.text for a in xml.find_all('loc')]
+        total = len(urls)
+        sys.stderr.write(f'found {total} urls\n')
+
+        results = []
+        with ThreadPoolExecutor(max_workers=t) as ex:
+            for r in tqdm(ex.map(self._check, urls), total=total, unit='url'):
+                results.append(r)
+                # print(r.get('path'))
+                # for k, v in r.items():
+                #     if k == 'path':
+                #         continue
+                #     print(f'  {k}: {v}')
+
+        if o:
+            with open(o, 'w') as output:
+                self._write_csv(results, output)
+        elif o == '-':
+            output = sys.stdout
+            self._write_csv(results, output)
+
+    # checks
+
+    @staticmethod
+    def check_code(r: Response, _):
+        """Response code must be ok"""
+        return r.status_code in OK_CODES
+
+    @staticmethod
+    def check_ttfb(r: Response, _):
+        """Time to first byte must be small"""
+        return r.elapsed.total_seconds() * 1000 < MAX_TTFB
+
+    @staticmethod
+    def check_len_title(_, p):
+        """Title length of page will be in range"""
+        return TITLE_MIN_LENGTH < len(p.title.text) < TITLE_MAX_LENGTH
+
+    @staticmethod
+    def check_len_desc(_, p):
+        """Description length of page will be in range"""
+        d = p.find('meta', {'name': 'description'}).get('content')
+        return DESCRIPTION_MIN_LENGTH < len(d) < DESCRIPTION_MAX_LENGTH
+
+    # /checks
 
     @staticmethod
     def _get_response(url):
@@ -40,54 +98,14 @@ class Seo:
         p_url = urlparse(url)
         path = f'{p_url.path}?{p_url.query}' if p_url.query else p_url.path
 
-        title = page.find('title').text
-        desc = page.find('meta', {'name': 'description'}).get('content')
-
-        return {
+        result = {
             'path': path,
-            'code': int(response.status_code in OK_CODES),
-            'ttfb': int(checks['ttfb'](response.elapsed.total_seconds() * 1000)),
-            'title': int(checks['len_title'](title)),
-            'description': int(checks['len_desc'](desc)),
         }
 
-    def check(self, sitemap_url, t=4, o=''):
-        """Performs base technical SEO check for urls from sitemap
+        for check_name, fn in self.checks.items():
+            result[check_name] = int(fn(response, page))
 
-        sitemap_url -- url pointing to sitemap.xml
-        t -- number of threads
-        Keep in mind: when more threads,
-        ttfb may be larger than MAX_TTFB and test will fail
-        """
-        sys.stderr.write('Check sitemap...\n')
-        xml = self._get_page(self._get_response(sitemap_url))
-        urls = [a.text for a in xml.find_all('loc')]
-        total = len(urls)
-        sys.stderr.write(f'found {total} urls\n')
-
-        results = []
-        with ThreadPoolExecutor(max_workers=t) as ex:
-            for r in tqdm(ex.map(self._check, urls), total=total, unit='url'):
-                results.append(r)
-
-        fail = 0
-        success = 0
-        for r in results:
-            for c in r.keys():
-                if c != 'path':
-                    b = r[c]
-                    if b:
-                        success += 1
-                    else:
-                        fail += 1
-
-        sys.stderr.write(f'Success: {success}, fail: {fail}.\n')
-        if o:
-            with open(o, 'w') as output:
-                self._write_csv(results, output)
-        elif o == '-':
-            output = sys.stdout
-            self._write_csv(results, output)
+        return result
 
     @staticmethod
     def _write_csv(results, output):
