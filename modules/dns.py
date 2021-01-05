945c99ce39c5ed43a221ca93ae663ce3486a2bd0
Mikhail Yudin <fagci.nsk@gmail.com>
2021-01-05 23:36:33 +0700

    added timeout, refactor

--- modules/dns.py
+++ modules/dns.py
@@ -14,54 +14,33 @@ ipv4_re = compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
 class Dns:
     """Perform DNS related actions"""
 
-    @staticmethod
-    def _get_nameservers() -> list:
-        url = 'https://public-dns.info/nameservers.csv'
-        lines = get(url).text.splitlines(False)
-        col_names = lines[0].split(',')
-        """
-        ip_address
-        name
-        as_number
-        as_org
-        country_code
-        city
-        version
-        error
-        dnssec
-        reliability
-        checked_at
-        created_at
-        """
-        items = []
-        for line in lines[1:]:
-            row = dict(zip(col_names, line.split(',')))
-            ip = row['ip_address']
-            if not ipv4_re.match(ip):
-                continue
-            if row.get('reliability') != '1.00':
-                continue
-            items.append(row)
-        return items
+    def __init__(self):
+        self.ns_source = 'https://public-dns.info/nameservers.csv'
 
     def _check(self, nameserver):
         resolver = Resolver(configure=False)
         resolver.nameservers = [nameserver['ip_address']]
+
         try:
             t = time()
             res: Answer = resolver.resolve(
-                self.domain_to_resolve, lifetime=0.1)
+                self.domain_to_resolve, lifetime=self.max_s)
             if len(res.rrset) > 0:
                 nameserver['time'] = round((time() - t) * 1000)
         except:
             pass
+
         return nameserver
 
-    def fastest(self, domain_to_resolve='ya.ru'):
+    def fastest(self, max_ms: int = 100, domain='ya.ru'):
         """Retrieves ipv4 dns nameservers,
-        and get fastest (<100ms) for your location/network."""
-        self.domain_to_resolve = domain_to_resolve
+        and get fastest (<100ms by default)
+        for your location/network."""
+
+        self.max_s = float(max_ms) / 1000.0
+        self.domain_to_resolve = domain
         nameservers = self._get_nameservers()
+
         with ThreadPoolExecutor() as executor:
             results = []
             for res in tqdm(executor.map(self._check, nameservers), total=len(nameservers)):
@@ -74,3 +53,24 @@ class Dns:
             name = res.get('name') or '-'
             tim = res.get('time') or '-'
             print(f'{ip:<15} {tim:>3} ms {name}')
+
+    def _get_nameservers(self) -> list:
+        """Retrieves nameservers info from source
+
+        Keys:
+        ip_address, name, as_number, as_org,
+        country_code, city, version, error, dnssec,
+        reliability, checked_at, created_at"""
+
+        lines = get(self.ns_source).text.splitlines(False)
+        col_names = lines[0].split(',')
+
+        items = []
+        is_ipv4 = ipv4_re.match
+        for line in lines[1:]:
+            row = dict(zip(col_names, line.split(',')))
+            ip = row['ip_address']
+            if is_ipv4(ip) and row.get('reliability') == '1.00':
+                items.append(row)
+
+        return items
