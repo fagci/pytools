f777c3f3fbc0c1e42a148df254892364edce30c2
Mikhail Yudin <fagci.nsk@gmail.com>
2021-01-05 19:37:34 +0700

    html sel, xpath commands

--- modules/html.py
+++ modules/html.py
@@ -6,18 +6,35 @@ from requests import get
 class Html:
     """HTML utilities"""
 
-    @staticmethod
-    def src(url, selector=None):
-        """Shows prettified html source,
-        or some part by selector if specified
+    def sel(self, url, selector):
+        """Shows some part of source by selector
 
         selector -- css selector, ex.: ul>li
         """
-        response = get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        if selector:
-            for res in soup.select(selector):
-                soup = BeautifulSoup(str(res), 'html.parser')
+        for res in self._soup(url).select(selector):
+            print(res.prettify())
+
+    def xpath(self, url, xpath):
+        """Shows some part of source by xpath
+
+        xpath -- ex.: //a/@href
+        """
+        for res in self._lxml(url).xpath(xpath):
+            if str(res).find('<') != -1:
+                soup = BeautifulSoup(res, 'html.parser')
                 print(soup.prettify())
-        else:
-            print(soup.prettify())
+            else:
+                print(res)
+
+    def src(self, url):
+        """Shows prettified html source,"""
+        print(self._soup(url).prettify())
+
+    @staticmethod
+    def _soup(url):
+        return BeautifulSoup(get(url).text, 'html.parser')
+
+    @staticmethod
+    def _lxml(url):
+        from lxml import html
+        return html.fromstring(get(url).text)
