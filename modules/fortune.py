e6075ee6b0add24aab4575cf6a7669fdc4304080
Mikhail Yudin <fagci.nsk@gmail.com>
2021-01-08 20:27:53 +0700

    better text, better namings, refactor

--- modules/fortune.py
+++ modules/fortune.py
@@ -15,6 +15,34 @@ class Fortune:
         self.ips_count = ips_count
         self.workers = t
 
+    def ftp(self):
+        """Gather FTP hosts with anonymous access"""
+        self._generate_ips()
+        self._leave_ips_with_port(21)
+        self._filter_service(check_anon)
+
+        for ip in self.ips:
+            print(ip)
+
+    def http(self):
+        """Gather hosts with site on 80 port"""
+        self._generate_ips()
+        self._leave_ips_with_port(80)
+        self._filter_service(check_http)
+
+        for _, ip, title in self.ips:
+            print(f'{ip:<15} {title}')
+            try:
+                FortuneModel.create(ip=ip, title=title)
+            except Exception as e:
+                print(e)
+
+    @staticmethod
+    def list():
+        """Get list of previous spins (ips with http title)"""
+        for item in FortuneModel.select().order_by(FortuneModel.title):
+            print('{}\t{}'.format(item.ip, item.title))
+
     @staticmethod
     def ip_list(count: int = 100):
         """Generates list of random ips"""
@@ -22,45 +50,19 @@ class Fortune:
             print(ip)
 
     def _generate_ips(self):
-        print('[*] Generating', self.ips_count, 'ips...')
+        print('[*] create generator of', self.ips_count, 'ips...')
         self.ips = generate_ips(self.ips_count)
 
-    def _leave_ips(self, port: int):
-        print('[*] Gathering ips for', port, 'port using',
+    def _leave_ips_with_port(self, port: int):
+        print('[*] Gathering ips with', port, 'port, using',
               (self.workers or '[default]'), 'workers...')
         self.ips = list(ips_with_port(
             self.ips, port, self.workers, total=self.ips_count))
         self.ips_count = len(self.ips)
-        print('Got', self.ips_count, 'ips for', port, 'port')
+        print('Got', self.ips_count, 'ips.')
 
-    def ftp(self):
-        """Gather FTP hosts with anonymous access"""
-        self._generate_ips()
-        self._leave_ips(21)
-        anons = list(filter_ips(self.ips, check_anon, total=self.ips_count))
-
-        for ip in anons:
-            print(ip)
-
-    def http(self):
-        """Gather hosts with site on 80 port"""
-        self._generate_ips()
-        self._leave_ips(80)
-
-        results = []
-        for _, ip, title in filter_ips(self.ips, check_http, total=self.ips_count):
-            try:
-                FortuneModel.create(ip=ip, title=title)
-            except Exception as e:
-                print(e)
-            results.append((ip, title))
-
-        print(len(results), 'results')
-        for ip, title in results:
-            print(f'{ip:<15} {title}')
-
-    @staticmethod
-    def list():
-        """Get list of previous spins (ips with http title)"""
-        for item in FortuneModel.select().order_by(FortuneModel.title):
-            print(f'{item.ip}\t{item.title}')
+    def _filter_service(self, fn):
+        print('[*] Filtering service:', self.ips_count, 'ips...')
+        self.ips = list(filter_ips(self.ips, fn, total=self.ips_count))
+        self.ips_count = len(self.ips)
+        print('Got', self.ips_count, 'ips.')
